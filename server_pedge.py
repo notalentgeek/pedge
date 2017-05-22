e18800df12c1ba53caf4f0f59f65ecb721554026
notalentgeek <mikael.pratama@yahoo.com>
2017-05-22 07:33:33 +0200

    Test.

--- server_pedge.py
+++ server_pedge.py
@@ -210,14 +210,14 @@ if __name__ == "__main__":
                     if i.get(global_var.name_column_dt) == input_latest:
                         name_client_temp = i.get(global_var.name_column_name_client)
                         face_temp        = database.get_doc_first_value(c_temp, input_latest, global_var.name_column_dt, global_var.name_column_value_detection, "{}_{}".format(global_var.name_table_face    , name_client_temp), name_db)
-                        pitch_temp       = database.get_doc_first_value(c_temp, input_latest, global_var.name_column_dt, global_var.name_column_value_detection, "{}_{}".format(global_var.name_table_pitch   , name_client_temp), name_db)
-                        presence_temp    = database.get_doc_first_value(c_temp, input_latest, global_var.name_column_dt, global_var.name_column_value_detection, "{}_{}".format(global_var.name_table_presence, name_client_temp), name_db)
-                        volume_temp      = database.get_doc_first_value(c_temp, input_latest, global_var.name_column_dt, global_var.name_column_value_detection, "{}_{}".format(global_var.name_table_volume  , name_client_temp), name_db)
+                        #pitch_temp       = database.get_doc_first_value(c_temp, input_latest, global_var.name_column_dt, global_var.name_column_value_detection, "{}_{}".format(global_var.name_table_pitch   , name_client_temp), name_db)
+                        #presence_temp    = database.get_doc_first_value(c_temp, input_latest, global_var.name_column_dt, global_var.name_column_value_detection, "{}_{}".format(global_var.name_table_presence, name_client_temp), name_db)
+                        #volume_temp      = database.get_doc_first_value(c_temp, input_latest, global_var.name_column_dt, global_var.name_column_value_detection, "{}_{}".format(global_var.name_table_volume  , name_client_temp), name_db)
                         i[global_var.name_column_dt] = dt;
                         if type(face_temp)     is not list: i[global_var.name_table_face]     = face_temp
-                        if type(pitch_temp)    is not list: i[global_var.name_table_pitch]    = pitch_temp
-                        if type(presence_temp) is not list: i[global_var.name_table_presence] = presence_temp
-                        if type(volume_temp)   is not list: i[global_var.name_table_volume]   = volume_temp
+                        #if type(pitch_temp)    is not list: i[global_var.name_table_pitch]    = pitch_temp
+                        #if type(presence_temp) is not list: i[global_var.name_table_presence] = presence_temp
+                        #if type(volume_temp)   is not list: i[global_var.name_table_volume]   = volume_temp
                         data_sent.append(i)
 
                 emit("sent_input", data_sent)
