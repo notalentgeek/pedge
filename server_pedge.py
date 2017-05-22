b8d68776bbed0e646d5de3ead1884650a5b1b717
notalentgeek <mikael.pratama@yahoo.com>
2017-05-22 07:40:57 +0200

    Test.

--- server_pedge.py
+++ server_pedge.py
@@ -222,12 +222,6 @@ if __name__ == "__main__":
                         #pitch_temp       = database.get_doc_first_value(c_temp, input_latest, global_var.name_column_dt, global_var.name_column_value_detection, "{}_{}".format(global_var.name_table_pitch   , name_client_temp), name_db)
                         #presence_temp    = database.get_doc_first_value(c_temp, input_latest, global_var.name_column_dt, global_var.name_column_value_detection, "{}_{}".format(global_var.name_table_presence, name_client_temp), name_db)
                         #volume_temp      = database.get_doc_first_value(c_temp, input_latest, global_var.name_column_dt, global_var.name_column_value_detection, "{}_{}".format(global_var.name_table_volume  , name_client_temp), name_db)
-                        i[global_var.name_column_dt] = dt;
-                        if type(face_temp)     is not list: i[global_var.name_table_face]     = face_temp
-                        #if type(pitch_temp)    is not list: i[global_var.name_table_pitch]    = pitch_temp
-                        #if type(presence_temp) is not list: i[global_var.name_table_presence] = presence_temp
-                        #if type(volume_temp)   is not list: i[global_var.name_table_volume]   = volume_temp
-                        data_sent.append(i)
 
                 print(data_sent)
                 emit("sent_input", data_sent)
